072f51749db721642c4c15e3983a428f85d08dee
ToivoSeppa <seppatoi@arcada.fi>
2022-09-26 12:08:47 +0300

    DB connection working!
    Co-authored-by: Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
+
+// Vi måste importera express-modulen för att skapa en express-app
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3030
 
-// Importera dotenv och läs in .env-filen
+// We import dotenv and .env-file
 require('dotenv').config()
+console.log(process.env.DOTENV_WORKS)
 
-// Skapa och öppna mongoose-connection till MongoDB Atlas
 mongoose.connect(process.env.DB_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
-db.on('open', () => console.log('Connected to DB'))
+db.on('open', () => console.log('Connected to database'))
 
-// Testa att detta funkar, ersätt sedan med egen kod
-app.get('/', (req, res) => res.json("Det funkar!"))
+app.use(express.json())
+
+// Middleware-funktioner
+const runAlways = (req, res, next) => {
+    res.locals.myVariable = 'hello from runAlways' // res.locals är ett bra ställe att spara lokala variabler
+    console.log(`A request was made to ${req.path}`)
+    next()
+}
+const runSometimes = (req, res, next) => {
+    console.log(`sometimes`)
+    next()
+}
+
+// app.use(middleWare) => denna körs för varje request
+app.use(runAlways)
+
+app.use('/static', express.static(__dirname + '/public'))
 
 const usersRouter = require('./routes/users')
 app.use('/users', usersRouter)
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+// app.use() också för att använda en router-modul
+
+// app.listen startar upp vår express-server och lyssnar på requests på en viss port.
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
