c13b75eefb60f0acfd5fdbd3a74a473ab8f5a154
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2022-09-29 17:33:54 +0300

    Added user validation for cabins in POST
    Co-authored-by: ToivoSeppa <ToivoSeppa@users.noreply.github.com>

--- routes/bookings.js
+++ routes/bookings.js
@@ -17,23 +17,32 @@ router.get('/', authToken, async (req, res) => {
 router.post('/', authToken, async (req, res) => {
     try {
 
+        try {
+            const cabinFound = await Cabin.findOne({ _id: req.body.cabin })
+        } catch (error) {
+            return res.send({ msg: "No such cabin in database" })
+        }
+
+        const book = await Booking.findOne({
+            cabin: req.body.cabin,
+            startdate: { $lt: req.body.enddate },
+            enddate: { $gt: req.body.startdate }
+        }).exec()
+        if (book) {
+            return res.send({ msg: "Selected time for booking not available" })
+        }
+
         const booking = new Booking({
-            creator: req.author.sub,
-            address: req.body.address,
+            author: req.author.sub,
+            cabin: req.body.cabin,
             startdate: req.body.startdate,
             enddate: req.body.enddate
+
         })
-        
-        const cabinExists = await Cabin.findOne({address: req.body.address}).exec()
 
-        if (cabinExists) {
-            
-            const newBooking = booking.save()
+        const newBooking = await booking.save()
 
-            res.send(newBooking)
-        } else {
-            res.send({ msg: "Cabin doesn't exist"})
-        }
+        res.send({ saved: newBooking })
 
     } catch (error) {
         res.status(500).send({ msg: error.message })
@@ -45,7 +54,7 @@ router.patch('/:id', authToken, async (req, res) => {
     try {
         // TODO: Fix req.cabin.sub 
         const updatedBooking = await Booking.findOneAndUpdate(
-            { _id: req.params.id , author: req.author.sub  },
+            { _id: req.params.id, author: req.author.sub },
             req.body,
             { new: true }
         )
